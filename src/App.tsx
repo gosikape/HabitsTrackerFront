2e6a0a258b124d77f1af767b76a6267a7d9ca6ad
Małgorzata O <gosiapater@yahoo.com>
2023-11-10 10:29:48 +0100

    Init commit - pushing project to remote repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Routes, Route, Navigate} from "react-router-dom";
+import {HabitView} from "./views/HabitView";
+import {LoginView} from "./views/LoginView";
+import {RegisterView} from "./views/RegisterView";
+import {SingleHabitView} from "./views/SingleHabitView";
+import {NotFoundView} from "./views/NotFoundView";
+import {UserView} from "./views/UserView";
+import {LogoutView} from "./views/LogoutView";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
 
-export default App;
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate replace to="/login"/>}/>
+            <Route path="/login" element={<LoginView/>}/>
+            <Route path="/register" element={<RegisterView/>}/>
+            <Route path="/habit" element={<HabitView/>}/>
+            <Route path="/habit/:habitId" element={<SingleHabitView/>}/>
+            <Route path="/user" element={<UserView/>}/>
+            <Route path="/logout" element={<LogoutView/>}/>
+            <Route path="*" element={<NotFoundView/>}/>
+        </Routes>
+    );
+};
+
